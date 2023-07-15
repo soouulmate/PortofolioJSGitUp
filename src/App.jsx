3ba8eb7a1460661b77b3442848b02c9c9d5df4b7
Ana Laura Montenegro Sánchez <78060614+soouulmate@users.noreply.github.com>
2023-07-15 16:41:25 +0000

    primeros cambios

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Laura Montenegro",
+  title: "Practica #1",
+  email: "lauraa.montenegros@gmail.com",
+  gitHub: "hola",
+  instagram: "jaja no",
+  linkedIn: "tampoco",
   medium: "",
-  twitter: "microsoft",
+  twitter: "PEOR",
   youTube: "Code",
 };
 
